e6a6fc380743abc654c06547e348975ac4c122a0
bayron <bayrontakeoch21@gmail.com>
2024-01-20 21:42:43 +0100

    feat(Hash):Creo la clase que crea el hash

--- FitFlavorLibreria/src/Encriptacion/Hash.java
+++ FitFlavorLibreria/src/Encriptacion/Hash.java
@@ -5,10 +5,36 @@
  */
 package Encriptacion;
 
+import java.security.MessageDigest;
+
 /**
  *
  * @author bayro
  */
 public class Hash {
 
+    public static String hashText(String texto) {
+        MessageDigest messageDigest;
+        byte hashedText[] = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            byte dataBytes[] = texto.getBytes();
+            messageDigest.update(dataBytes);
+            hashedText = messageDigest.digest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(hashedText);
+    }
+
+    public static String hexadecimal(byte[] hashedText) {
+        char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        StringBuffer buf = new StringBuffer();
+        for (int j = 0; j < hashedText.length; j++) {
+            buf.append(hexDigit[(hashedText[j] >> 4) & 0x0f]);
+            buf.append(hexDigit[hashedText[j] & 0x0f]);
+        }
+        return buf.toString();
+    }
 }
