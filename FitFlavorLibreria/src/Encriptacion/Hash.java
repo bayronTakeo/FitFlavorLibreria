c863735ef829b846d0a1717dee22f4ebdfead172
bayron <bayrontakeoch21@gmail.com>
2024-01-21 20:50:05 +0100

    fix(encriptacion):CAmbio el hash al tipo SHA-256

--- FitFlavorLibreria/src/Encriptacion/Hash.java
+++ FitFlavorLibreria/src/Encriptacion/Hash.java
@@ -1,40 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Encriptacion;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
-/**
- *
- * @author bayro
- */
 public class Hash {
 
     public static String hashText(String texto) {
-        MessageDigest messageDigest;
-        byte hashedText[] = null;
         try {
-            messageDigest = MessageDigest.getInstance("MD5");
-            byte dataBytes[] = texto.getBytes();
-            messageDigest.update(dataBytes);
-            hashedText = messageDigest.digest();
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = messageDigest.digest(texto.getBytes(StandardCharsets.UTF_8));
+
+            // Convertir a representación hexadecimal
+            StringBuilder hexStringBuilder = new StringBuilder();
+            for (byte hashedByte : hashedBytes) {
+                hexStringBuilder.append(String.format("%02X", hashedByte));
+            }
+
+            return hexStringBuilder.toString();
         } catch (Exception e) {
             e.printStackTrace();
+            return null; // Manejo de errores
         }
-        return new String(hashedText);
     }
 
-    public static String hexadecimal(byte[] hashedText) {
-        char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        StringBuffer buf = new StringBuffer();
-        for (int j = 0; j < hashedText.length; j++) {
-            buf.append(hexDigit[(hashedText[j] >> 4) & 0x0f]);
-            buf.append(hexDigit[hashedText[j] & 0x0f]);
-        }
-        return buf.toString();
-    }
 }
